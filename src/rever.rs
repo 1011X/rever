52092ce176a5c9ffbf89def542f2e6186ba3f929
1011X <1011XXXXX@gmail.com>
2017-02-02 14:24:00 -0500

    grammar.bnf: 	* removed number suffixes for now 	* require array length; forget about dynamically-sized structs for now 	* removed optional trailing commas. might add again later 	* use `var` for mutable arguments instead of `mut` 	* use `var` for mutable variables instead of `let mut` 	* created binary expressions to use at `if` and `from` statements 	* changed bit-not from using `~` to `!` 	* removed `rev if` for now. most likely will add again later 	* removed incomplete `for .. in` part; doesn't have a way to execute code in back-loop like `from .. until ..` can. 	* simplified some parts
    rever.rs:
    	* put ast structs/enums closer to their parsers
    	* removed lifetimes and will use heap-allocated values for now
    	* removed a lot of redundant/unnecessary code/stuff
    	* commented out the invert method for statements; don't even know if I'll need it
    	* used `factor` instead of `expression` in some parts. I still consider expressions to be very expensive in code length and memory because of their tree-structure and requirement to reset the memory along the way, but I'll keep thinking about whether to keep them or not.
    	* statement are terminated with a ';' instead of just whitespace or a comma
    	* some other simplifications

--- src/rever.rs
+++ src/rever.rs
@@ -1,151 +1,250 @@
-/*
 use std::str;
-use nom::{
-	multispace, space,
-	alpha, digit,
-};
 
-#[derive(Debug)]
-pub struct Pattern<'a>(bool, &'a str);
+named!(ident, re_bytes_find!("^[A-Za-z_][A-Za-z0-9_]*"));
+named!(constant<Num>, map_res!(digit, str::from_utf8));
 
 #[derive(Debug, Clone, Copy)]
-pub enum Type { U16, I16, Usize, Isize }
-
-#[derive(Debug)]
-pub struct Procedure<'a> {
-	name: &'a str,
-	args: Vec<(Pattern<'a>, Type)>,
-	code: Vec<Statement<'a>>,
+pub enum Type {
+	Composite(String, Vec<Type>),
+	Bool,
+	U16, I16, Usize, Isize,
+	Array(Box<Type>, usize),
+	Pointer(Box<Type>),
+	Proc(Vec<Type>),
 }
 
+named!(type_<Type>, alt!(
+	map!(tag!("bool"), |_| Type::Bool)
+	| map!(tag!("u16"), |_| Type::U16)
+	| map!(tag!("i16"), |_| Type::I16)
+	| map!(tag!("usize"), |_| Type::Usize)
+	| map!(tag!("isize"), |_| Type::Isize)
+	| ws!(do_parse!(
+		tag!("[") >>
+		t: type_ >>
+		tag!(";") >>
+		n: num >>
+		tag!("]") >>
+		
+		(Type::Array(t, n))
+	))
+	| map!(ws!(preceded!(tag!("^"), type_)), |t| Type::Pointer(Box::new(t)))
+));
+
 #[derive(Debug)]
-pub enum Item<'a> {
-	//Static(bool, &'a str, Type, Expression<'a>),
+pub struct Procedure {
+	name: String,
+	args: Vec<(bool, String, Type)>,
+	code: Vec<Statement>,
+}
+
+named!(proc_<Procedure>, ws!(do_parse!(
+	tag!("proc") >>
+	name: ident >>
+	args: ws!(delimited!(
+		tag!("("),
+		ws!(separated_list!(
+			tag!(","),
+			ws!(do_parse!(
+				m: opt!(tag!("var")) >>
+				name: ident >>
+				tag!(":") >>
+				t: type_ >>
+				
+				(m, name, t)
+			))
+		)),
+		tag!(")")
+	)) >>
+	code: block >>
+	
+	// some extra work can be done here if it's really needed
+	(Procedure {
+		name: name,
+		args: args,
+		code: code.into_iter()
+			.collect(),
+	})
+)));
+
+
+#[derive(Debug)]
+pub enum Item {
+	//Static(bool, String, Type, ConstExpr),
 	//Mod(Vec<Item>),
-	Proc(Procedure<'a>)
+	Proc(Procedure),
+}
+
+named!(item<Item>, map!(proc_, Item::Proc));
+
+enum Factor {
+	Lit(Literal),
+	LVal(LValue),
+}
+
+named!(factor<Factor>, alt!(
+	map!(lit, Factor::Lit)
+	| map!(lval, Factor::Lval)
+));
+
+enum Literal {
+	Num(Number),
+}
+
+named!(lit<Literal>, map!(num, Literal::Num));
+
+enum Number {
+	Unknown(u16),
+	U16(u16),
+	I16(i16),
+	Usize(usize),
+	Isize(isize),
+}
+
+named!(num<Number>, alt!(
+	re_bytes_find!("^[-+]?[1-9][0-9]*")
+	| re_bytes_find!("^0x[A-Fa-f0-9_]+")
+	| re_bytes_find!("^0b[01_]+")
+));
+
+struct LValue {
+	id: String,
+	ops: Vec<Deref>,
+}
+
+named!(lval<LValue>, ws!(do_parse!(
+	ident: id >>
+	ops: ws!(many0!(alt!(
+		map!(tag!("*"), |_| Deref::Direct)
+		| ws!(delimited!(
+			tag!("["),
+			map!(factor, Deref::Indexed),
+			tag!("]")
+		))
+	))) >>
+	
+	(LValue {
+		id: ident,
+		ops: ops,
+	})
+)));
+
+enum Deref {
+	Direct,
+	Indexed(Factor),
 }
 
 #[derive(Debug)]
-pub enum Expression<'a> {
-	Variable(&'a str),
-	Constant(&'a str, Option<Type>),
+pub enum Statement {
+	Let(String, Option<Type>, Expression),
+	Var(String, Option<Type>, Expression),
+	Drop(String, Option<Type>, Expression),
+	
+	//Not(String),
+	//Neg(String),
+	
+	RotLeft(String, Factor),
+	RotRight(String, Factor),
+	
+	CCNot(String, Factor, Factor),
+	Xor(String, Vec<Factor>),
+	
+	Add(String, Vec<(bool, Factor)>),
+	Sub(String, Vec<(bool, Factor)>),
+	
+	Swap(LValue, LValue),
+	CSwap(Factor, LValue, LValue),
+	
+	//If(BinExpr, Vec<Statement>, Option<Vec<Statement>>, BinExpr),
+	
+	//From(BinExpr, Option<Vec<Statement>>, Option<Vec<Statement>>, BinExpr),
+	
+	Call(LValue, Vec<Factor>),
+	Uncall(LValue, Vec<Factor>),
+	
+	//Switch(String, Vec<String, Vec<Statement>>),
+	//Unsafe(Vec<Statement>),
 }
 /*
-#[derive(Debug)]
-enum Test { Parity, Sign }
-*/
-
-type Var<'a> = (String, Option<Expression<'a>>);
-
-#[derive(Debug)]
-pub enum Statement<'a> {
-	//Not(&'a str),
-	//Neg(&'a str),
-	Call(&'a str, Vec<Expression<'a>>),
-	Uncall(&'a str, Vec<Expression<'a>>),
-	RotLeft(&'a str, Expression<'a>),
-	RotRight(&'a str, Expression<'a>),
-	Swap(&'a str, &'a str),
-	Xor(&'a str, Expression<'a>),
-	Add(&'a str, Expression<'a>),
-	Sub(&'a str, Expression<'a>),
-	CCNot(&'a str, Expression<'a>, Expression<'a>),
-	CSwap(Expression<'a>, &'a str, &'a str),
-	Let(Pattern<'a>, Option<Type>, Expression<'a>),
-	Delete(Pattern<'a>, Option<Type>, Expression<'a>),
-	//If(Test, &'a str, Vec<Statement<'a>>),
-	//Switch(&'a str, Vec<&'a str, Vec<Statement<'a>>>),
-	//From(Test, &'a str, Vec<Statement<'a>>),
-	//Unsafe(Vec<Statement<'a>>),
-}
-
-impl<'a> Statement<'a> {
+impl Statement {
 	pub fn invert(self) -> Self {
 		match self {
-			//Statement::Not(..)         => self,
-			//Statement::Neg(..)         => self,
-			Statement::Call(a, b)      => Statement::Uncall(a, b),
-			Statement::Uncall(a, b)    => Statement::Call(a, b),
-			Statement::RotLeft(a, b)   => Statement::RotRight(a, b),
-			Statement::RotRight(a, b)  => Statement::RotLeft(a, b),
-			Statement::Swap(..)        => self,
-			Statement::Xor(..)         => self,
-			Statement::Add(a, b)       => Statement::Sub(a, b),
-			Statement::Sub(a, b)       => Statement::Add(a, b),
-			Statement::CSwap(..)       => self,
-			Statement::Let(a, b, c)    => Statement::Delete(a, b, c),
-			Statement::Delete(a, b, c) => Statement::Let(a, b, c),
+			Statement::Let(..)        => self,
+			Statement::Var(a, b, c)   => Statement::Drop(a, b, c),
+			Statement::Drop(a, b, c)  => Statement::Var(a, b, c),
+			
+			//Statement::Not(..)        => self,
+			//Statement::Neg(..)        => self,
+			
+			Statement::RotLeft(a, b)  => Statement::RotRight(a, b),
+			Statement::RotRight(a, b) => Statement::RotLeft(a, b),
+			
+			Statement::CCNot(..)      => self,
+			Statement::Xor(..)        => self,
+			
+			Statement::Add(a, b)      => Statement::Sub(a, b),
+			Statement::Sub(a, b)      => Statement::Add(a, b),
+			
+			Statement::Swap(..)       => self,
+			Statement::CSwap(..)      => self,
+			
+			Statement::Call(a, b)     => Statement::Uncall(a, b),
+			Statement::Uncall(a, b)   => Statement::Call(a, b),
+			
 			// ...
-			Statement::Unsafe(..)      => self,
+			Statement::Unsafe(..)     => self,
 		}
 	}
 }
-
-named!(identifier<&str>, map_res!(alpha, str::from_utf8));
-named!(constant<&str>, map_res!(digit, str::from_utf8));
-/*
-named!(whitespace, alt!(
-	multispace |
-	do_parse!(tag!("/*") >> c: many0!(anychar) >> tag!("*/"), || c) |
-	do_parse!(tag!("//") >> c: many0!(anychar) >> tag!("\n"), || c) |
-	whitespace
-));
 */
-named!(expression<Expression>, alt!(
-	map!(identifier, Expression::Variable)
-	| do_parse!(
-		c: constant >>
-		t: opt!(preceded!(tag!("_"), parse_type)) >>
-		(Expression::Constant(c, t))
-	)
-));
 
-named!(parse_type<Type>, alt!(
-	map!(tag!("u16"),     |_| Type::U16)
-	| map!(tag!("i16"),   |_| Type::I16)
-	| map!(tag!("usize"), |_| Type::Usize)
-	| map!(tag!("isize"), |_| Type::Isize)
-));
-
-named!(parse_pattern<Pattern>, ws!(do_parse!(
-	m: opt!(tag!("mut")) >>
-	var: identifier >>
-	
-	(Pattern(m.is_some(), var))
-)));
-
-named!(tuple<Vec<Expression> >, ws!(delimited!(
+named!(args<Vec<Factor> >, ws!(delimited!(
 	tag!("("),
 	ws!(separated_list!(
 		tag!(","),
-		expression
+		factor
 	)),
 	tag!(")")
 )));
 
-named!(parse_block<Vec<Option<Statement>>>, ws!(delimited!(
+named!(block<Vec<Statement>>, ws!(delimited!(
 	tag!("{"),
-	ws!(many0!(ws!(do_parse!(
-		stmt: opt!(parse_statement) >>
-		alt!(tag!(",") | tag!("\n") | tag!("\r\n")) >>
-		(stmt)
-	)))),
+	ws!(many0!(
+		ws!(terminated!(stmt, tag!(";")))
+	)),
 	tag!("}")
 )));
 
-named!(parse_statement<Statement>, alt!(
-	ws!(do_parse!(
-		tag!("!") >>
-		var: identifier >>
-		(Statement::Not(var))
+named!(stmt<Statement>, alt!(
+	ws!(preceded!(tag!("!"), map!(ident, Statement::Not))
+	| ws!(preceded!(tag!("-"), map!(ident, Statement::Neg))
+	| ws!(do_parse!(
+		left: ident >>
+		tag!("<>") >>
+		right: ident >>
+		
+		(Statement::Swap(left, right))
 	))
 	| ws!(do_parse!(
-		tag!("-") >>
-		var: identifier >>
-		(Statement::Neg(var))
+		dest: ident >>
+		tag!("^=") >>
+		lctrl: expression >>
+		tag!("&") >>
+		rctrl: expression >>
+		
+		(Statement::CCNot(dest, lctrl, rctrl))
 	))
 	| ws!(do_parse!(
-		l: identifier >>
+		control: expression >>
+		tag!("?") >>
+		left: ident >>
+		tag!("<>") >>
+		right: ident >>
+		
+		(Statement::CSwap(control, left, right))
+	))
+	| ws!(do_parse!(
+		l: ident >>
 		m: alt!(
 			tag!("+=") | tag!("-=") |
 			tag!("^=") |
@@ -162,30 +261,7 @@ named!(parse_statement<Statement>, alt!(
 			_ => unreachable!()
 		})
 	))
-	| ws!(do_parse!(
-		left: identifier >>
-		tag!("<>") >>
-		right: identifier >>
-		(Statement::Swap(left, right))
-	))
-	| ws!(do_parse!(
-		dest: identifier >>
-		tag!("^=") >>
-		lctrl: expression >>
-		tag!("&") >>
-		rctrl: expression >>
-		
-		(Statement::CCNot(dest, lctrl, rctrl))
-	))
-	| ws!(do_parse!(
-		control: expression >>
-		tag!("?") >>
-		left: identifier >>
-		tag!("<>") >>
-		right: identifier >>
-		
-		(Statement::CSwap(control, left, right))
-	))
+	/*
 	| ws!(do_parse!(
 		tag!("unsafe") >>
 		b: parse_block >>
@@ -195,81 +271,70 @@ named!(parse_statement<Statement>, alt!(
 			.collect()
 		))
 	))
+	*/
 	| ws!(do_parse!(
-		name: identifier >>
-		args: tuple >>
+		tag!("call") >>
+		name: ident >>
+		args: args >>
 		(Statement::Call(name, args))
-	)) |
-	do_parse!(
-		tag!("rev") >> space >> name: identifier >> space? >> args: tuple,
-		|| Statement::Uncall(name, args)
-	)
+	))
+	| ws!(do_parse!(
+		tag!("uncall") >>
+		name: ident >>
+		args: args >>
+		(Statement::Uncall(name, args))
+	))
 	| ws!(do_parse!(
 		tag!("let") >>
-		name: parse_pattern >>
-		ty: opt!(ws!(do_parse!(
-			tag!(":") >>
-			ty: parse_type >>
-			
-			(ty)
-		))) >>
+		name: ident >>
+		ty: opt!(ws!(preceded!(tag!(":"), type_))) >>
 		tag!("=") >>
-		e: expression,
+		e: expression >>
 		
 		(Statement::Let(name, ty, e))
 	))
 	| ws!(do_parse!(
-		tag!("delete") >>
-		name: parse_pattern >>
-		ty: opt!(ws!(do_parse!(
-			tag!(":") >>
-			ty: parse_type >>
-			
-			(ty)
-		)) >>
+		tag!("var") >>
+		name: ident >>
+		ty: opt!(ws!(preceded!(tag!(":"), type_))) >>
 		tag!("=") >>
 		e: expression >>
 		
-		(Statement::Delete(name, ty, e))
+		(Statement::Var(name, ty, e))
+	))
+	| ws!(do_parse!(
+		tag!("drop") >>
+		name: ident >>
+		tag!("=") >>
+		e: expression >>
+		
+		(Statement::Drop(name, None, e))
+	))
+	| ws!(do_parse!(
+		tag!("if") >>
+		p: expr >>
+		t: block >>
+		e: opt!(ws!(preceded!(tag!("else"),  block))) >>
+		tag!("assert") >>
+		a: expr >>
+		
+		(Statement::If(p, t, e, a))
+	))
+	| ws!(do_parse!(
+		tag!("from") >>
+		a: expr >>
+		d: opt!(block) >>
+		l: opt!(block) >>
+		tag!("until") >>
+		p: expr >>
+		
+		(Statement::From(a, d, l, p))
 	))
 ));
 
-named!(parse_procedure<Procedure>, ws!(do_parse!(
-	tag!("proc") >>
-	name: identifier >>
-	args: ws!(delimited!(
-		tag!("("),
-		ws!(separated_list!(
-			tag!(","),
-			ws!(do_parse!(
-				pat: parse_pattern >>
-				tag!(":") >>
-				ty: parse_type >>
-				
-				(pat, ty)
-			))
-		)),
-		tag!(")")
-	)) >>
-	code: parse_block >>
-	
-	// some extra work can be done here if it's really needed
-	(Procedure {
-		name: name,
-		args: args,
-		code: code.into_iter()
-			.filter_map(|s| s)
-			.collect(),
-	})
-)));
-
-named!(parse_item<Item>, alt!(
-	ws!(do_parse!(p: parse_procedure >> (Item::Proc(p))))
+named!(pub program<Vec<Item> >, complete!(
+	ws!(many0!(parse_item))
 ));
-
-named!(pub parse_program<Vec<Item> >, complete!(ws!(
-	many0!(parse_item)
-)));
 /*
 fn main() {
 	println!("{:?}", parse_statement(b"a <> b"));
@@ -345,189 +410,3 @@ fn main() {
 	}"));
 }
 */
-*/
-
-named!(id, re_bytes_find!("^[A-Za-z_][A-Za-z0-9_]*"));
-named!(mod_op, alt!(
-	tag!("+=")
-	| tag!("-=")
-	| tag!("^=")
-	| tag!("<<=")
-	| tag!(">>=")
-));
-
-enum Statement {
-	Let(bool, String, Option<Type>, Expr),
-	Reset(String, Expr),
-	Swap(String, String),
-	Add(String, Option<Expr>, Expr),
-	Sub(String, Option<Expr>, Expr),
-	Xor(String, Option<Expr>, Expr),
-	LRot(String, Option<Expr>, Expr),
-	RRot(String, Option<Expr>, Expr),
-	If(Expr, Block, Option<Block>, Expr),
-	From(Expr, Option<Block>, Option<Block>, Expr),
-	Call(String, Vec<Expr>),
-	Uncall(String, Vec<Expr>),
-}
-named!(stmt<Statement>, alt!(
-	ws!(do_parse!(
-		tag!("let") >>
-		m: opt!(tag!("mut")) >>
-		n: id >>
-		t: opt!(ws!(preceded!(tag!(":"), typ))) >>
-		tag!("=") >>
-		e: expr >>
-		
-		(Statement::Let(m.is_some(), n, t, e))
-	))
-	| ws!(do_parse!(
-		tag!("reset") >>
-		n: id >>
-		tag!("=") >>
-		e: expr >>
-		
-		(Statement::Reset(n, e))
-	))
-	| ws!(map!(
-		separated_pair!(id, tag!("<>"), id),
-		|(l, r)| Statement::Swap(l, r)
-	))
-	| ws!(do_parse!(
-		v: id >>
-		s: opt!(ws!(delimited!(
-			tag!("["),
-			expr,
-			tag!("]")
-		))) >>
-		o: alt!(
-			tag!("+=")
-			| tag!("-=")
-			| tag!("^=")
-			| tag!("<<=")
-			| tag!(">>=")
-		) >>
-		e: expr >>
-		
-		(match o {
-			b"+=" => Statement::Add(v, s, e),
-			b"-=" => Statement::Sub(v, s, e),
-			b"^=" => Statement::Xor(v, s, e),
-			b"<<=" => Statement::LRot(v, s, e),
-			b">>=" => Statement::RRot(v, s, e),
-			_ => unreachable!()
-		})
-	))
-	| ws!(do_parse!(
-		tag!("if") >>
-		p: expr >>
-		t: block >>
-		e: opt!(ws!(preceded!(tag!("else"),  block))) >>
-		tag!("assert") >>
-		a: expr >>
-		
-		(Statement::If(p, t, e, a))
-	))
-	| ws!(do_parse!(
-		tag!("from") >>
-		a: expr >>
-		d: opt!(block) >>
-		l: opt!(block) >>
-		tag!("until") >>
-		p: expr >>
-		
-		(Statement::From(a, d, l, p))
-	))
-	| ws!(do_parse!(
-		c: alt!(tag!("call") | tag!("uncall")) >>
-		//r: opt!(tag!("rev")) >>
-		n: id >>
-		a: ws!(delimited!(
-			tag!("("),
-			ws!(separated_list!(tag!(","), expr)),
-			tag!(")")
-		)) >>
-		
-		(match c {
-			b"call" => Statement::Call(n, a),
-			b"uncall" => Statement::Uncall(n, a),
-			_ => unreachable!()
-		})
-	))
-))
-
-named!(block<Block>, ws!(delimited!(
-	tag!("{"),
-	ws!(many0!(
-		ws!(terminated!(stmt, tag!(";")))
-	)),
-	tag!("}")
-)));
-
-re_bytes_find!("^[-+]?[1-9][0-9]*")
-re_bytes_find!("^0x[A-Fa-f0-9_]+")
-re_bytes_find!("^0b[01_]+")
-
-
-
-<num-type> ::= 'u16'
-	| 'i16'
-	| 'usize'
-	| 'isize'
-
-<literal> ::= <num>
-	| '[' <value> ']'
-
-<typelist> ::= <type> | <type> ',' <typelist>
-<type> ::= <id> ['<' <typelist> '>']
-	| 'bool'
-	| 'u16'
-	| 'i16'
-	| 'usize'
-	| 'isize'
-	| '[' <type> [';' <num>] ']'
-	| '^' <type>
-	| <proc-type>
-
-<proc-type> ::= 'proc' '(' <proc-param-type-list> ')'
-<proc-param-type-list> ::= [<proc-param-type-list'>]
-<proc-param-type-list'> ::= ['mut'] <type> | ['mut'] <type> ',' <proc-param-type-list'>
-
-<param> ::= ['mut'] <id> ':' <type>
-
-<paramlist> ::= [<paramlist'>]
-<paramlist'> ::= <param>
-	| <param> ',' <paramlist'>
-
-<value> ::= <num> | <id>
-
-<arglist> ::= [<arglist'>]
-<arglist'> ::= <value>
-	| <value> ',' <arglist'>
-
-<const-bin-op> ::= '*' | '/' | '%' | <bin-op>
-<bin-op> ::= '+' | '-' | '^' | '&' | '|' | '<' | '>' | '<=' | '>=' | '=' | '!=' | '<<' | '>>'
-<mod-op> ::= '+=' | '-=' | '^=' | '<<=' | '>>='
-
-<stmt> ::= 'let' ['mut'] <id> [':' <type>] '=' <expr>
-	| 'reset' <id> '=' <expr>
-	| 'const' <id> ':' <type> '=' <const-expr>
-	| <id> ['[' <expr> ']'] <mod-op> <expr>
-	| <id> '<>' <id>
-	| <if-stmt>
-	| <from-stmt>
-	| 'call' <id> '(' <arglist> ')'
-	| 'uncall' <id> '(' <arglist> ')'
-
-<stmtlist> ::= [<stmt> ';' <stmtlist>]
-
-<if-stmt> ::=
-	'if' <expr> '{' <stmtlist> '}'
-	['else' '{' <stmtlist> '}']
-	'assert' <expr>
-
-<from-stmt> ::=
-	'from' <expr> ['{' <stmtlist> '}']
-	'until' <expr> ['{' <stmtlist> '}']
-
-<proc> ::= 'proc' <id> '(' <paramlist> ')' '{' <stmtlist> '}'
