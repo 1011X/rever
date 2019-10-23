0095b5ec34e92fa7a4e287fd34541b8eae18eaf3
1011X <1011XXXXX@gmail.com>
2019-10-22 20:38:55 -0400

    rm janus files from index, odd gl/git behavior differences

--- src/tokenize.rs
+++ src/tokenize.rs
@@ -1,30 +1,31 @@
-use std::char;
-
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Token {
     // keywords
-    Proc, Do, Undo, From, Until, Loop, If, Then, Else, Fi, Let, Var, Drop,
+    Proc, Do, Undo, From, Until, Loop, If, Then, Else, Fi, Let, Var, Drop, Mod,
     // unused
-    Fn, Return, Match, As, //Goto, ComeFrom,
+    Fn, Return, Match, As, For, In, //Goto, ComeFrom,
     
     // brackets
     LParen, RParen, LBracket, RBracket, LBrace, RBrace,
     
     // single purpose
-    Not, And, Or, Swap,
+    Swap,
     // relational
-    Neq, Lt, Gt,
+    Neq, Lt, Gt, Lte, Gte,
+    // rotation
+    Rol, Ror,
     
     // multi-purpose
-    Eq, Comma, Colon, Caret, Add, Sub, Rol, Ror,
+    Eq, Comma, Colon, At, Add, Sub, Semicolon, Period,
+    Bang, Assign, Caret,
     
-    Space(usize),
     Comment(String),
     
     // ident and number
     Ident(String),
     Number(String),
-    //String(String),
+    //Char(String),
+    String(String),
 }
 
 /*
@@ -39,111 +40,174 @@ order of ops:
 */
 
 
-pub fn tokenize(mut s: &str) -> Result<Vec<Token>, &str> {
-    let mut tokens = Vec::new();
+pub fn tokenize(s: &str) -> Result<Vec<Token>, &'static str> {
+    let mut tokens = Vec::with_capacity(s.len());
+    let mut chars = s.chars().peekable();
     
-    loop {
-        // handle identifiers and keywords
-        // [_A-Za-z]
-        if s.starts_with(|c: char| c == '_' || c.is_ascii_alphabetic()) {
-            let mut i = 1;
-            
-            while s[i..].starts_with(|c: char| c == '_' || c.is_ascii_alphanumeric()) {
-                i += 1;
+    while let Some(c) = chars.next() {
+        tokens.push(match c {
+            // identifiers and keywords
+            // [_A-Za-z]
+            '_' | 'A'..='Z' | 'a'..='z' => {
+                let mut token = String::with_capacity(1);
+                token.push(c);
+                
+                // [_A-Za-z0-9]
+                while let Some(&c) = chars.peek() {
+                    if c == '_' || c.is_ascii_alphanumeric() {
+                        token.push(chars.next().unwrap());
+                    }
+                    else {
+                    	token.shrink_to_fit();
+                        break;
+                    }
+                }
+                
+                match &*token {
+                    // keywords
+                    "proc"  => Token::Proc,
+                    "do"    => Token::Do,
+                    "undo"  => Token::Undo,
+                    "from"  => Token::From,
+                    "until" => Token::Until,
+                    "loop"  => Token::Loop,
+                    "if"    => Token::If,
+                    "then"  => Token::Then,
+                    "else"  => Token::Else,
+                    "fi"    => Token::Fi,
+                    "let"   => Token::Let,
+                    "var"   => Token::Var,
+                    "drop"  => Token::Drop,
+                    "mod"   => Token::Mod,
+                    
+                    "fn"     => Token::Fn,
+                    "return" => Token::Return,
+                    "match"  => Token::Match,
+                    "as"     => Token::As,
+                    "for"    => Token::For,
+                    "in"     => Token::In,
+
+                    _ => Token::Ident(token)
+                }
+            }
+
+            // handle numbers
+            // [0-9]
+            '0'..='9' => {
+                let mut token = String::with_capacity(1);
+                token.push(c);
+				
+                // [0-9]*
+                while let Some('0'..='9') = chars.peek() {
+                    token.push(chars.next().unwrap());
+                }
+                
+                token.shrink_to_fit();
+                Token::Number(token)
             }
             
-            tokens.push(match &s[..i] {
-                // keywords
-                "proc"  => Token::Proc,
-                "do"    => Token::Do,
-                "undo"  => Token::Undo,
-                "from"  => Token::From,
-                "until" => Token::Until,
-                "loop"  => Token::Loop,
-                "if"    => Token::If,
-                "then"  => Token::Then,
-                "else"  => Token::Else,
-                "fi"    => Token::Fi,
-                "let"   => Token::Let,
-                "var"   => Token::Var,
-                "drop"  => Token::Drop,
-                
-                "fn"    => Token::Fn,
-                "return" => Token::Return,
-                "match" => Token::Match,
-                "as"    => Token::As,
-                
-                // operations
-                "not" => Token::Not,
-                
-                "and" => Token::And,
-                "or"  => Token::Or,
-                //"xor" => Token::Xor,
-                
-                id => Token::Ident(id.to_string())
-            });
+            // handle strings
+            '"' => {
+            	let mut string = String::new();
+            	
+            	loop {
+            		match chars.next() {
+            			Some('"') => break,
+            			Some('\\') => string.push(match chars.next() {
+            				Some('\\') => '\\',
+            				Some('"')  => '"',
+            				Some('n')  => '\n',
+            				Some('t')  => '\t',
+            				Some('0')  => '\0',
+            				
+	            			Some(_) | None =>
+	            				return Err("eof @ escaped character")
+            			}),
+            			Some(c) => string.push(c),
+            			None => return Err("eof @ string"),
+            		}
+        		}
+        		
+        		string.shrink_to_fit();
+        		Token::String(string)
+            }
+
+            '!' => match chars.peek() {
+                Some('=') => {chars.next(); Token::Neq}
+                _ => Token::Bang
+            }
+            '<' => match chars.peek() {
+                Some('>') => {chars.next(); Token::Swap}
+                //Some('=') => {chars.next(); Token::Lte}
+                _ => Token::Lt
+            }
+            '>' => match chars.peek() {
+                Some('=') => {chars.next(); Token::Gte}
+                //_ => Token::Gt
+                _ => return Err("eof @ `>=`")
+            }
+
+            '(' => Token::LParen,
+            ')' => Token::RParen,
+            '[' => Token::LBracket,
+            ']' => Token::RBracket,
+            '{' => Token::LBrace,
+            '}' => Token::RBrace,
+            ',' => Token::Comma,
+            '.' => Token::Period,
+            ':' => match chars.peek() {
+                Some('=') => {chars.next(); Token::Assign}
+                //_ => Token::Gt
+                _ => Token::Colon
+            }
+            ';' => Token::Semicolon,
+            '=' => Token::Eq,
+            '+' => Token::Add,
+            '-' => Token::Sub,
+            '^' => Token::Caret,
             
-            s = &s[i..];
-        }
-        // handle numbers
-        // [0-9]
-        else if s.starts_with(|c: char| c.is_ascii_digit()) {
-            let mut i = 1;
-            
-            while s[i..].starts_with(|c: char| c.is_ascii_digit()) {
-                i += 1;
+            '≠' => Token::Neq,
+            //'≤' => Token::Lte,
+            '≥' => Token::Gte,
+
+            // space
+            // TODO should newlines have their own token?
+            ' ' | '\t' | '\n' => continue,
+
+            // comment
+            '/' => match chars.peek() {
+                Some('/') => {
+                    chars.next();
+                    let mut comment = String::new();
+                    
+                    while let Some(c) = chars.next() {
+                        if c == '\n' { break }
+                        else { comment.push(c) }
+                    }
+                    
+                    comment.shrink_to_fit();
+                    Token::Comment(comment)
+                }
+                _ => return Err("//")
             }
             
-            tokens.push(Token::Number(s[..i].to_string()));
-            s = &s[i..];
-        }
-        else if s.starts_with(|c: char| c.is_ascii_punctuation()) {
-            
-        }
-        // whitespace
-        // [ \t\n\f\c]
-        else if s.starts_with(|c: char| c.is_ascii_whitespace()) {
-            s = &s[1..];
-        }
-        else if s.starts_with(|c: char| c.is_ascii_punctuation()) {
-            tokens.push(match &s[..1] {
-                "(" => Token::LParen,
-                ")" => Token::RParen,
-                "[" => Token::LBracket,
-                "]" => Token::RBracket,
-                "{" => Token::LBrace,
-                "}" => Token::RBrace,
-                "," => Token::Comma,
-                ":" => Token::Colon,
-                "=" => Token::Eq,
-                "!" => match &s[1..2] {
-                    "=" => Token::Neq,
-                    _ => return Err("!=")
-                }
-                "+" => Token::Add,
-                "-" => Token::Sub,
-                
-                "<" => Token::Lt,
-                /*
-                "<" => match &s[1..2] {
-                    ">" => Token::Swap,
-                    "=" => Token::Lte,
-                    _ => Err("")
-                }
-                */
-                //"<<" => Token::Rol,
-                
-                ">" => Token::Gt,
-                //">>" => Token::Ror,
-                //">=" => Token::Gte,
-                
-                _ => return Err("unrecognized symbol")
-            });
-        }
-        else if s.is_empty() {
-            break;
-        }
+            _ => return Err("unrecognized symbol")
+        });
     }
     
+    tokens.shrink_to_fit();
     Ok(tokens)
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	
+	#[test]
+	fn keywords() {
+		assert_eq!(tokenize("proc"), Ok(vec![Token::Proc]));
+		assert_eq!(tokenize("  proc  "), Ok(vec![Token::Proc]));
+		assert_eq!(tokenize("do"), Ok(vec![Token::Do]));
+		assert_eq!(tokenize("undo"), Ok(vec![Token::Undo]));
+	}
 }
